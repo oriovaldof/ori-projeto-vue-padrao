c5c3fd3cb4f57d67ced2081f17e2f612215ee381
Oriovaldo Fialho <oriovaldof@gmail.com>
2022-08-22 19:20:12 -0400

    servindo a aplicacao vue de producao por meio do express

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
 
+app.use(express.static(__dirname+'/dist/'));
+
 //responder a requisições feitas para o servidor web por meio da rota get
 app.get('/', function(req, res){
-    res.send('Olá, eu sou um servidor web em Node com Express');
+    // res.send('Olá, eu sou um servidor web em Node com Express');
+    res.sendFile(__dirname+'/dist/index.html');
 });
 
 app.listen(3000,function(){
