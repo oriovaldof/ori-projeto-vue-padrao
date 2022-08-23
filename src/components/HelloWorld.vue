ff483ec112a8dc42e1572a01d88a5b20de3e39a4
Oriovaldo Fialho <oriovaldof@gmail.com>
2022-08-23 19:22:35 -0400

    variaveis de ambiente

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,12 @@
 <template>
-  <h1>Componente Ola Mundo</h1>
+  <h1>Componente Ola Mundo {{variavel}}</h1>
 </template>
 
 <script>
 export default {
+  data:()=>({
+    variavel: process.env.VUE_APP_MINHA_VARIAVEL_DE_AMBIENTE
+  }),
   name: 'HelloWorld',
   
 }
