7d4ed3a9070fe6a70a03509191b561dac86c985f
Oriovaldo Fialho <oriovaldof@gmail.com>
2022-08-23 19:54:32 -0400

    variaveis de ambientes por tipo de ambiente

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,17 @@
 <template>
+<div>
   <h1>Componente Ola Mundo {{variavel}}</h1>
+  <p>Link da API: {{url_api}}</p>
+  <p>Modo: {{modo}}</p>
+</div>
 </template>
 
 <script>
 export default {
   data:()=>({
-    variavel: process.env.VUE_APP_MINHA_VARIAVEL_DE_AMBIENTE
+    variavel: process.env.VUE_APP_MINHA_VARIAVEL_DE_AMBIENTE,
+    url_api: process.env.VUE_APP_API_URL,
+    modo: process.env.VUE_APP_MODO
   }),
   name: 'HelloWorld',
   
